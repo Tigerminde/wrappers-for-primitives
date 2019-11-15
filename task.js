7753838dcd718b5b8f4575be9185b26767ee2fda
Tigerminde <tevanyanjan85@mail.ru>
2019-11-15 09:07:58 +0300

    wraperprimitive

---
+++ task.js
@@ -0,0 +1,95 @@
+function calculateMortgage() {
+    let percent = window.percent.value;
+    let contribution = window.contribution.value;
+    let amount = window.amount.value;
+    let date = window.date.value;
+
+    let result = calculateTotalMortgage(percent, contribution, amount, date);
+    let span = window.mortageResult;
+    span.textContent = result;
+}
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+     
+    
+    // код для задачи №1 писать здесь
+    
+    
+    function replaceComma (percent) {
+        let re = /,/;
+        let percentWithoutComma = percent.replace(re, '.');
+        console.log(percentWithoutComma);
+        return percentWithoutComma;
+
+    };
+
+
+    let P = +(parseFloat(replaceComma(percent))/100/12);
+    console.log(P);
+
+    function checkisNumber( percent, contribution, amount) {
+        let arr = { percent, contribution, amount };
+        for (let prop in arr) {
+            if (isNaN(arr[prop])) {
+                console.log(`Параметр ${prop} содержит неправильное значение ${arr[prop]}`);
+            }
+        }
+    }
+    
+    checkisNumber(percent, contribution, amount);
+
+    function checkDays(date){
+        let result;
+        if (((new Date(date).getDate())- new Date().getDate()) < 0) {
+            result = -1;
+        } else { result = 0 } ;
+        return result;
+    }
+    
+    const calcMonths = () => {
+        let monthOfYear = ((((new Date(date)).getFullYear()) - (new Date().getFullYear())) * 12);
+        let months = ((new Date(date).getMonth()) - (new Date().getMonth())); 
+        return (monthOfYear + months + checkDays(date)) ;
+    }
+
+    let n = calcMonths();
+    console.log(`Срок кредита, мес = ${n}`);
+
+    const checkAmount = () => {
+        if ((amount-contribution) >= 0) { return (amount - contribution);
+        } else { console.log(`Сумма кредита указана неверно, получилось: ${amount - contribution} `)
+        }
+    }    
+        
+    let monthlyPayment = +( checkAmount() * ( P + P /((Math.pow (( 1 + P ), n )-1))) ).toFixed(2);
+    
+    console.log(`Ежемесячный платеж составит: ${monthlyPayment}`);
+    
+    let totalAmount = (monthlyPayment * n).toFixed(2);
+    let totalAll = (+(totalAmount + contribution)).toFixed(2);
+    console.log(`Сумма всех оплат составит ${totalAll} руб.`);
+
+    return totalAmount;
+};
+
+
+function sayHello() {
+    let name = window.personName.value;
+    let greeting = getGreeting(name);
+    let span = window.helloResult;
+    span.textContent = greeting;
+}
+
+    
+    // код для задачи №2 писать здесь
+
+    
+function getGreeting(name) {
+   function getName (name) {
+        if ((name === "") || (name === "undefined") || (!Boolean(name))) { 
+            return "Аноним";
+            } else { 
+                return name };
+   };
+   return (`Привет, мир! Меня зовут ${getName(name)}`);
+}
